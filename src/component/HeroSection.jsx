0cf918f93d8d747356f843323d16c83173b2c9ff
Divyyanshu <divvyyanshuutailor@gmail.com>
2024-12-21 15:39:12 +0530

    hero section done

--- src/component/HeroSection.jsx
+++ src/component/HeroSection.jsx
@@ -49,3 +49,38 @@ const HeroSection = () => {
 }
 
 export default HeroSection;
+
+
+// import React from 'react'
+// import HeroImage from '../assets/image/hero-section-image.png'
+// import SlopImage from '../assets/image/slope.svg'
+// import NavButton from '../component/buttons/NavButton';
+// import LearnButton from '../component/buttons/LearnButton';
+
+// const HeroSection = () => {
+//     return (
+//         <>
+//             <div className='flex gap-20 items-center'>
+//                 <div className='py-20'>
+//                     <h1 className='text-5xl uppercase font-semibold italic text-left'>The <span className='text-[#0b9ad6]'> body </span>achieves,
+//                         <br />
+//                         what the <span className='text-[#0b9ad6]'> mind </span>
+//                         believes.</h1>
+//                     <p className='text-left text-lg font-normal text-nowrap pt-2 tracking-tighter leading-6'>Having a perfect body requires a lot of training. Nice-looking body and powerful organism are
+//                         <br />
+//                         interconnected - and we can help you with both.
+//                     </p>
+//                     <div className='flex pt-4'>
+//                     <NavButton/>
+//                     <LearnButton/>
+//                     </div>
+//                 </div>
+//                 <div>
+//                     <img src={HeroImage} alt="" className='h-[70vh]' />
+//                 </div>
+//             </div>
+//         </>
+//     )
+// }
+
+// export default HeroSection
